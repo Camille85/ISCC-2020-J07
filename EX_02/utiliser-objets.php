cd226341dee9984108c85c06928adbb9c525b9a5
Camille85 <67278268+Camille85@users.noreply.github.com>
2020-07-02 18:52:33 +0200

    Jour 7

--- EX_02/utiliser-objets.php
+++ EX_02/utiliser-objets.php
@@ -7,7 +7,7 @@ echo $date1->format('d/m/Y H:i:s');
     </p>
 
 <?php
-$date2 = '20/06/2020 9:00';
+$date2 = '22/06/2020 9:00';
 $dateTime = DateTime::createFromFormat('d/m/Y H:i', $date2);
 echo $dateTime->format('d/m/Y H:i') ;
 ?>
@@ -15,10 +15,10 @@ echo $dateTime->format('d/m/Y H:i') ;
 <p>
 
 <?php
-$debut = new DateTime('2020-06-20'); 
+$debut = new DateTime('2020-06-22'); 
 $fin = new DateTime();
 $interval = $debut->diff($fin);
-echo $interval->format('Il s\'est écoulé  %R%d jours et %R%H heure. ');
+echo $interval->format('Il s\'est écoulé %R%d jours, %R%H heure et %R%i minutes. ');
 ?>
 </p>
 
